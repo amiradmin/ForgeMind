5658d6e82a4f9a3c867e05287e813f2605cb8864
Amir <amirbehvandi747@gmail.com>
2026-07-11 11:14:24 +0330

    feat(api): implement Asset CRUD API

--- backend/apps/assets/api/serializers/__init__.py
+++ backend/apps/assets/api/serializers/__init__.py
@@ -1,4 +1,5 @@
 from .area import AreaSerializer
+from .asset import AssetSerializer
 from .organization import OrganizationSerializer
 from .plant import PlantSerializer
 
@@ -6,4 +7,5 @@ __all__ = [
     "OrganizationSerializer",
     "PlantSerializer",
     "AreaSerializer",
+    "AssetSerializer",
 ]
